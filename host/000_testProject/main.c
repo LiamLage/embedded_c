4fe293ad966faacd0d0f32334756a2307284bc02
Liam Lage <44103555+LiamLage@users.noreply.github.com>
2021-10-23 02:14:24 +0200

    Add first projects host & target

---
+++ host/000_testProject/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 23 Oct 2021
+ *      Author: Liam Lage
+ */
+#include <stdio.h>
+
+int main(void) {
+	printf("%s\n", "hello world!");
+	printf("Press 'Enter' to exit application.\n");
+	getchar();
+	return 0;
+}
+
